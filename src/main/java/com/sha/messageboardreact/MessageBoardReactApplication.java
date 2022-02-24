d87bbf885a6ac4ccd00b78b1341b429617d4de5e
shajahanvzm <shajahanvzm@gmail.com>
2022-02-24 19:41:51 +0530

    first commit

---
+++ src/main/java/com/sha/messageboardreact/MessageBoardReactApplication.java
@@ -0,0 +1,13 @@
+package com.sha.messageboardreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageBoardReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageBoardReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sha/messageboardreact/MessageBoardReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sha.messageboardreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageBoardReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
